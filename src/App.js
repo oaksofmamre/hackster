f8ddbd6e2bcf6667d91d2b8d4325b651e138274b
DC <heyyodc-git@yahoo.com>
2018-02-03 22:03:43 -0800

    renders on browser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+const title = "Hackster";
+
+const list = [
+  {
+    title: "Build Yourself a Redux",
+    url: "https://zapier.com/engineering/how-to-build-redux/",
+    author: "jdeal",
+    num_comments: 111,
+    points: 111,
+    objectID: 0
+  },
+  {
+    title: "Mern: Build JavaScript apps using React and Redux",
+    url: "http://mern.io/",
+    author: "sinkensabe",
+    num_comments: 222,
+    points: 222,
+    objectID: 1
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{title}</h2>
+        {list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+          </div>
+        ))}
       </div>
     );
   }
