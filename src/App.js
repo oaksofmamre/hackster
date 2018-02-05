1e087380ffb14ed7bcc0590315c31820016f1a95
DC <heyyodc-git@yahoo.com>
2018-02-05 15:48:26 -0800

    refactor

--- src/App.js
+++ src/App.js
@@ -57,57 +57,49 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <form>
-        <input
-          type="text"
-          value={value}
-          // placeholder="Type in your search"
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
+function Search({ value, onChange, children }) {
+  return (
+    <form>
+      {children}
+      <input
+        type="text"
+        value={value}
+        placeholder="Type in your search"
+        onChange={onChange}
+      />
+    </form>
+  );
 }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span> {item.author}</span>
-            <span> {item.num_comments}</span>
-            <span> {item.points} </span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
-              {/* open up browser's console to see the different effects */}
-              {/* <Button onClick={console.log(item.objectID)}>Dismiss</Button> */}
-              {/* <Button onClick={() => console.log(item.objectID)}> Dismiss </Button> */}
-            </span>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function Table({ list, pattern, onDismiss }) {
+  return (
+    <div>
+      {list.filter(isSearched(pattern)).map(item => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span> {item.author}</span>
+          <span> {item.num_comments}</span>
+          <span> {item.points} </span>
+          <span>
+            <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+            {/* open up browser's console to see the different effects */}
+            {/* <Button onClick={console.log(item.objectID)}>Dismiss</Button> */}
+            {/* <Button onClick={() => console.log(item.objectID)}> Dismiss </Button> */}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-class Button extends Component {
-  render() {
-    const { onClick, className, children } = this.props;
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>
-    );
-  }
+function Button({ onClick, className = "", children }) {
+  return (
+    <button onClick={onClick} className={className} type="button">
+      {children}
+    </button>
+  );
 }
 
 export default App;
