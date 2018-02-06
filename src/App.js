74f0849cf7760f5765751fb3870742a497928b7e
DC <heyyodc-git@yahoo.com>
2018-02-05 19:56:14 -0800

    sucessful API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const title = "Hackster";
+const DEFAULT_QUERY = "";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
 
-const list = [
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+// console.log(url);
+
+const title = "Hackster";
+/* const list = [
   {
     title: "Build Yourself a Redux",
     url: "https://zapier.com/engineering/how-to-build-redux/",
@@ -20,20 +27,46 @@ const list = [
     points: 222,
     objectID: 1
   }
-];
+]; */
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+// const isSearched = searchTerm => item =>
+// item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const isSearched = searchTerm => {
+  if (!searchTerm) {
+    return item => item.title;
+  } else {
+    return item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      searchTerm: ""
+      // list,
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss(id) {
@@ -48,14 +81,25 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    // const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) {
+      return null;
+    }
+
     return (
       <div className="App">
         <div>
           <h2>{title}</h2>
           <Search value={searchTerm} onChange={this.onSearchChange} />
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        {/* <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} /> */}
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
@@ -76,7 +120,7 @@ const Search = ({ value, onChange, children = "" }) => (
 const Table = ({ list, pattern, onDismiss }) => (
   <div>
     {list.filter(isSearched(pattern)).map(item => (
-      <div key={item.objectID} className="table-row">
+      <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
         </span>
